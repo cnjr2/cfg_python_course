f68ee74664b31df625eaabd3a8d48efb16ed0849
Charles Ravarani <c.ravarani@gmail.com>
2016-02-01 10:00:32 +0000

    making a git repo

---
+++ session_2/ex13_1.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, first, second, third = argv
+
+age = raw_input("How old are you? ")
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+print "Your age is:", age
